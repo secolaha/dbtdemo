369adef9a60fdc2d89243f57a3d49c66f354532e
Lars Hansson <lars.hansson@gmail.com>
2025-08-27 15:19:02 +0000

    fixed date

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -6,9 +6,7 @@ HOUR(to_timestamp(STARTED_AT)) as  HOUR_STARTED_AT,
 DAYNAME(to_timestamp(STARTED_AT)) as DAYOFWEEK,
 {{get_daytype('STARTED_AT')}} DAY_TYPE,
 MONTH(to_timestamp(STARTED_AT)),
-{{get_season('STARTED_AT')}} as STATION_OF_YEAR,
-{{get_daytype('STARTED_AT')}} as DAY_TYPE
-
+{{get_season('STARTED_AT')}} as STATION_OF_YEAR
 from 
 {{ source('demo', 'bike') }}
 where STARTED_AT != 'started_at'
